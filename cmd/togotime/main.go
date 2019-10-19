55752555964d120163780b71f9878badd160a0e6
Roxie Gibson <me@roxxers.xyz>
2019-10-19 15:33:46 +0100

    Added dotenv config support

--- cmd/togotime/main.go
+++ cmd/togotime/main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"togotime"
+
+	"github.com/caarlos0/env/v6"
+	"github.com/joho/godotenv"
 )
 
+type config struct {
+	APIToken string `env:"API_TOKEN,required"`
+}
+
 func main() {
-	h := togotime.NewAPIClient("2136a22845735689d7a6e7f15e56e087")
+	// Load config into env, then load those variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+
+	fmt.Printf("%+v\n", cfg)
+	h := togotime.NewAPIClient(cfg.APIToken)
 	fmt.Printf("%+v\n", h.Workspaces[0].Projects)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module togotime
 
 go 1.12
+
+require (
+	github.com/caarlos0/env/v6 v6.1.0
+	github.com/joho/godotenv v1.3.0
+)
